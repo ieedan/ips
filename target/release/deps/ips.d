f9d9d71830d2b958e67da5207c65bb73a4d84738
Aidan Bleser <aidanbleser35@gmail.com>
2023-06-01 11:27:55 -0500

    Updated Git Ingore

---
+++ target/release/deps/ips.d
@@ -0,0 +1,5 @@
+C:\Users\ablesea\Documents\VsCodeProjects\ips\target\release\deps\ips.exe: src\main.rs
+
+C:\Users\ablesea\Documents\VsCodeProjects\ips\target\release\deps\ips.d: src\main.rs
+
+src\main.rs:
---
+++ target/release/ips.d
@@ -0,0 +1,1 @@
+C:\Users\ablesea\Documents\GitHub\ips\target\release\ips.exe: C:\Users\ablesea\Documents\GitHub\ips\src\main.rs
